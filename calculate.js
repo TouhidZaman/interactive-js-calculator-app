ace28d59c08887edc882202111f1ef1bf1fa0b4b
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-02-13 04:43:56 +0600

    Icon click clc-button id not found Issue fixed

--- calculate.js
+++ calculate.js
@@ -2,26 +2,34 @@ const resultField = document.getElementById('result-field');
 const expressionField = document.getElementById('expression-field');
 let expression  = '';
 
-function insertMe(event) {
-    let number = event.target.innerText;
-    console.log(event.target.innerText);
-    expression += number ? number : event.target.id;
+//For inserting user input into expression
+const insertMe = (event) => {
+    let target = event.target;
+    //To fix Icon click clc-button id not found Issue
+    expression += target.innerText ? target.innerText :
+     target.id ? target.id : target.parentNode.id;
+     
     expressionField.innerText = expression;
-    // console.log(expression);
 }
-function clearDisplay() {
+
+//To clear claculator display
+const clearDisplay = () => {
     resultField.innerText = '0';
     expressionField.innerText = '0';
     expression = '';
 }
-function deleteItem() {
+
+//To delete a single value from expression
+const deleteItem = () => {
     expression = expression.slice(0, -1);
     if(expression.length > 0) {
         expressionField.innerText = expression;
     }
     else expressionField.innerText = '0';
 }
-function calculate() {
+
+//To get the final result from expression
+const calculate = () => {
     let result = eval(expression);
     resultField.innerText = result;
 }
