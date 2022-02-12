ba64d7d1967f8c1b74b42c3338a8089005c27ee0
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-02-13 03:18:09 +0600

    Calculate functionality added

--- calculate.js
+++ calculate.js
@@ -1,7 +1,27 @@
-function hello() {
-    alert('hello from javascript');
-}
+const resultField = document.getElementById('result-field');
+const expressionField = document.getElementById('expression-field');
+let expression  = '';
 
 function insertMe(event) {
-    // alert(event.target.innerText)
+    let number = event.target.innerText;
+    console.log(event.target.innerText);
+    expression += number ? number : event.target.id;
+    expressionField.innerText = expression;
+    // console.log(expression);
+}
+function clearDisplay() {
+    resultField.innerText = '0';
+    expressionField.innerText = '0';
+    expression = '';
+}
+function deleteItem() {
+    expression = expression.slice(0, -1);
+    if(expression.length > 0) {
+        expressionField.innerText = expression;
+    }
+    else expressionField.innerText = '0';
+}
+function calculate() {
+    let result = eval(expression);
+    resultField.innerText = result;
 }
